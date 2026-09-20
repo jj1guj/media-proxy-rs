d30906ebd12a3eb5e9e3eec35d3812d11391a7c0
jj1guj <jj1guj@jj1guj.net>
2026-09-20 20:53:08 +0900

    RISC-V向けリンク時にlibunwindをlibgccで補完

--- crossfiles/build.sh
+++ crossfiles/build.sh
@@ -5,7 +5,9 @@ else
 	source /app/crossfiles/${TARGETARCH}/${TARGETVARIANT}.sh
 fi
 export RUSTFLAGS="${RUSTFLAGS} -C target-feature=-crt-static -C link-self-contained=no -L native=/vips-system-lib -L native=/vips/lib -C link-arg=-fuse-ld=mold -C link-arg=-Wl,-rpath-link,/vips-system-lib -C link-arg=-Wl,-rpath-link,/vips/lib -C link-arg=-Wl,--allow-shlib-undefined"
-printf 'GROUP ( /vips-system-lib/libgcc_s.so.1 %s )\n' "$("${CC}" -print-libgcc-file-name)" > /vips-system-lib/libgcc_s.so
+libgcc_path="$("${CC}" -print-libgcc-file-name)"
+printf 'GROUP ( /vips-system-lib/libgcc_s.so.1 %s )\n' "${libgcc_path}" > /vips-system-lib/libgcc_s.so
+printf 'GROUP ( /vips-system-lib/libgcc_s.so.1 %s )\n' "${libgcc_path}" > /vips-system-lib/libunwind.so
 mkdir -p /musl/${MUSL_NAME}/dav1d /musl/${MUSL_NAME}/lcms2
 cp -r /dav1d/lib /musl/${MUSL_NAME}/dav1d/lib
 cp -r /lcms2/lib /musl/${MUSL_NAME}/lcms2/lib
